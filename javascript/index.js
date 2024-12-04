f28c31237fb00f8e10c653673368d2ce66d71bec
Lodeme101 <lodeme101@gmail.com>
2024-12-04 14:40:29 +0900

    Added functions for innerHTML with real time info

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,47 @@
 function updateTime() {
   //Porta
   let portaElement = document.querySelector("#porta");
-  let portaDateElement = portaElement.querySelector(".date");
-  let portaTimeElement = portaElement.querySelector(".time");
-  let portaTime = moment().tz("Portugal");
-  portaDateElement.innerHTML = portaTime.format("MMMM Do YYYY");
-  portaTimeElement.innerHTML = portaTime.format("h:mm:ss [<small>]A[</small>]");
+  if (portaElement) {
+    let portaDateElement = portaElement.querySelector(".date");
+    let portaTimeElement = portaElement.querySelector(".time");
+    let portaTime = moment().tz("Portugal");
+    portaDateElement.innerHTML = portaTime.format("MMMM Do YYYY");
+    portaTimeElement.innerHTML = portaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //Pretoria
   let pretoriaElement = document.querySelector("#pretoria");
-  let pretoriaDateElement = pretoriaElement.querySelector(".date");
-  let pretoriaTimeElement = pretoriaElement.querySelector(".time");
-  let pretoriaTime = moment().tz("Africa/Johannesburg");
-  pretoriaDateElement.innerHTML = pretoriaTime.format("MMMM Do YYYY");
-  pretoriaTimeElement.innerHTML = pretoriaTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  if (pretoriaElement) {
+    let pretoriaDateElement = pretoriaElement.querySelector(".date");
+    let pretoriaTimeElement = pretoriaElement.querySelector(".time");
+    let pretoriaTime = moment().tz("Africa/Johannesburg");
+    pretoriaDateElement.innerHTML = pretoriaTime.format("MMMM Do YYYY");
+    pretoriaTimeElement.innerHTML = pretoriaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )}<small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
